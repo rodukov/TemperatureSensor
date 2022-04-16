30e1689cbee465636c271cf49f7e1e129af2469c
rodukov <electroneuphoria@gmail.com>
2022-04-16 18:12:41 +0300

    I2C

---
+++ i2c_display/i2c_display.ino
@@ -0,0 +1,15 @@
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.print("Hello world...");
+
+}
+
+void loop() {
+
+}
